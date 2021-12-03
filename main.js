5b822e76c1350ef5afe938929b8a81d11ae47abd
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-03 21:08:09 +0900

    ✅  uppdate del all,time

--- main.js
+++ main.js
@@ -1,57 +1,70 @@
-const items =document.querySelector('.items');
-const input = document.querySelector('.footer_input');
-const addbtn = document.querySelector('.footer_button');
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+const alldel = document.querySelector('.delall');
 
-function onadd(){
-    //1.사용자가 입력한 텍스트를 받아온다.
-    const text= input.value;
-    if(text === ''){
-        input.focus();
-        return;
-    }
-    //2.새로운 아이템을만듬
-    const item =createItem(text);
-    // //3. 아이템컨테이너에 새로만든아이템을 추가한다
-    items.appendChild(item);
-    //4.새로 추가된 아이템으로 이동(스크롤링)
-    item.scrollIntoView({block:'center'});
 
-    // //5.인풋을 초기화한다
-    input.value= '';
+  const time =new Date();
+  let year = time.getFullYear();
+  let month = ('0' + (time.getMonth() + 1)).slice(-2);
+  let day = ('0' + time.getDate()).slice(-2);
+  let dateString = year + '-' + month  + '-' + day;
+
+
+function onAdd() {
+  const text = input.value;
+  if (text === '') {
     input.focus();
+    return;
+  }
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({ block: 'center' });
+  input.value = '';
+  input.focus();
 }
-let id = 0; //UUID 
-function createItem(text){
-    const itemRow =document.createElement('li');
-    itemRow.setAttribute('class','item_row');
-    itemRow.setAttribute('data-id',id);
-    itemRow.innerHTML = ` 
-    <div class="item">
-        <span class="item_name">${text}</span>
-        <button class="item_delete">
-            <i class="fas fa-trash-alt" data-id=${id}></i>
-        </button>
-    </div>
-    <div class="divider"></div>
-    `;
 
-    id++;
-    
-    return itemRow;
+let id = 0; // UUID
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+        <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete">
+                <i class="fas fa-trash-alt" data-id=${id}></i>
+            </button>
+        </div>
+        <span class="time">${dateString}</span>
+        <div class="item__divider"></div>`;
+  id++;
+  return itemRow;
 }
-addbtn.addEventListener('click',()=>{
-onadd();
+
+addBtn.addEventListener('click', () => {
+  onAdd();
 });
 
-input.addEventListener('keypress', event=>{
-    if(event.key === 'Enter'){
-        onadd();
-    }
+input.addEventListener('keypress', event => {
+  if (event.key === 'Enter') {
+    onAdd();
+  }
 });
-items.addEventListener('click', event =>{
-    const id = event.target.dataset.id;
-    if(id){
-        const toBeDelete =document.querySelector(`.item_row[data-id="${id}"]`);
-        toBeDelete.remove();
-    }
+
+items.addEventListener('click', event => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
+
+alldel.addEventListener('click',()=>{
+  const delete_all = document.querySelector('.items');
+  while (delete_all.hasChildNodes()) {	// 부모노드가 자식이 있는지 여부를 알아낸다
+  delete_all.removeChild(
+  delete_all.firstChild
+  );
+}
 });
