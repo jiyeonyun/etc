e77575f5dbd8354ac138747629365408ea187192
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-22 16:02:20 +0900

    완성!!

--- onlineshopping/src/main.js
+++ onlineshopping/src/main.js
@@ -2,6 +2,10 @@ const itemsul = document.querySelector('.items');
 const tshirts = document.querySelector('.t');
 const pants = document.querySelector('.p');
 const skirts = document.querySelector('.s');
+const logo = document.querySelector('.logo');
+const blue = document.querySelector('.blues');
+const yellow = document.querySelector('.yellows');
+const pink = document.querySelector('.pinks');
 //fetch the items
 function loadItems(){
     return fetch('data/data.json')
@@ -13,7 +17,7 @@ loadItems()
 .then(items=>{
   for(let i = 0 ; i<items.length; i++){
     const li = document.createElement('li');
-    li.setAttribute('class',`item ${items[i].type}`);
+    li.setAttribute('class',`item ${items[i].type} ${items[i].color}`);
     const thumnail = document.createElement('img');
     thumnail.setAttribute('class','item__thumbnail');
     thumnail.setAttribute('src',`${items[i].image}`);
@@ -24,16 +28,104 @@ loadItems()
     li.appendChild(describtion);
     itemsul.appendChild(li);
   }
+  logo.addEventListener('click',()=>{
+    const removethisgmain = document.querySelectorAll('.tshirt');
+    for(let i = 0; i < removethisgmain.length; i++){
+      removethisgmain[i].classList.remove('invisible');
+      }
+    const removethisg = document.querySelectorAll('.pants');
+    for(let i = 0; i < removethisg.length; i++){
+      removethisg[i].classList.remove('invisible');
+    }
+    const removethisg2 = document.querySelectorAll('.skirt');
+    for(let i = 0; i < removethisg2.length; i++){
+      removethisg2[i].classList.remove('invisible');
+    }
+  });
+  tshirts.addEventListener('click',()=>{
+  const removethisgmain = document.querySelectorAll('.tshirt');
+  for(let i = 0; i < removethisgmain.length; i++){
+    removethisgmain[i].classList.remove('invisible');
+    }
+  const removethisg = document.querySelectorAll('.pants');
+  for(let i = 0; i < removethisg.length; i++){
+    removethisg[i].classList.add('invisible');
+  }
+  const removethisg2 = document.querySelectorAll('.skirt');
+  for(let i = 0; i < removethisg2.length; i++){
+    removethisg2[i].classList.add('invisible');
+  }
+  });
+  pants.addEventListener('click',()=>{
+    const removethisgmain = document.querySelectorAll('.pants');
+    for(let i = 0; i < removethisgmain.length; i++){
+      removethisgmain[i].classList.remove('invisible');
+      }
+    const removethisg = document.querySelectorAll('.tshirt');
+    for(let i = 0; i < removethisg.length; i++){
+      removethisg[i].classList.add('invisible');
+    }
+    const removethisg2 = document.querySelectorAll('.skirt');
+    for(let i = 0; i < removethisg2.length; i++){
+      removethisg2[i].classList.add('invisible');
+    }
+  });
+  skirts.addEventListener('click',()=>{
+    const removethisgmain = document.querySelectorAll('.skirt');
+    for(let i = 0; i < removethisgmain.length; i++){
+      removethisgmain[i].classList.remove('invisible');
+      }
+    const removethisg = document.querySelectorAll('.tshirt');
+    for(let i = 0; i < removethisg.length; i++){
+      removethisg[i].classList.add('invisible');
+    }
+    const removethisg2 = document.querySelectorAll('.pants');
+    for(let i = 0; i < removethisg2.length; i++){
+      removethisg2[i].classList.add('invisible');
+    }
+  });
+  blue.addEventListener('click',()=>{
+    const removethisgmain = document.querySelectorAll('.blue');
+    for(let i = 0; i < removethisgmain.length; i++){
+      removethisgmain[i].classList.remove('invisible');
+      }
+    const removethisg = document.querySelectorAll('.yellow');
+    for(let i = 0; i < removethisg.length; i++){
+      removethisg[i].classList.add('invisible');
+    }
+    const removethisg2 = document.querySelectorAll('.pink');
+    for(let i = 0; i < removethisg2.length; i++){
+      removethisg2[i].classList.add('invisible');
+    }
+  });
+  yellow.addEventListener('click',()=>{
+    const removethisgmain = document.querySelectorAll('.yellow');
+    for(let i = 0; i < removethisgmain.length; i++){
+      removethisgmain[i].classList.remove('invisible');
+      }
+    const removethisg = document.querySelectorAll('.pink');
+    for(let i = 0; i < removethisg.length; i++){
+      removethisg[i].classList.add('invisible');
+    }
+    const removethisg2 = document.querySelectorAll('.blue');
+    for(let i = 0; i < removethisg2.length; i++){
+      removethisg2[i].classList.add('invisible');
+    }
+  });
+  pink.addEventListener('click',()=>{
+    const removethisgmain = document.querySelectorAll('.pink');
+    for(let i = 0; i < removethisgmain.length; i++){
+      removethisgmain[i].classList.remove('invisible');
+      }
+    const removethisg = document.querySelectorAll('.yellow');
+    for(let i = 0; i < removethisg.length; i++){
+      removethisg[i].classList.add('invisible');
+    }
+    const removethisg2 = document.querySelectorAll('.blue');
+    for(let i = 0; i < removethisg2.length; i++){
+      removethisg2[i].classList.add('invisible');
+    }
+  });
 })
 .catch(console.log('no'))
 
-tshirts.addEventListener('click',()=>{
-  const list = document.querySelector('li');
-  list.classList.contains(pants).setAttribute('class','item invisible ')
-});
-pants.addEventListener('click',()=>{
-
-});
-skirts.addEventListener('click',()=>{
-
-});
