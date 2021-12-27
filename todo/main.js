fbd2a665f486896d1dcb8bd993ef554cdc482b21
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-27 22:53:27 +0900

    saved Username

--- todo/main.js
+++ todo/main.js
@@ -4,14 +4,26 @@ const loginBtn = loginForm.querySelector('button');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    console.log(username);
-    greeting.innerText = "Hello "+username;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener('submit',onLoginSubmit);
+function paintGreetings(username){
+    greeting.innerText =`Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit',onLoginSubmit);
+}
+else{
+    paintGreetings(savedUsername)
+}
