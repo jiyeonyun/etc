4a2c8df1c8a54000857301bc8cdeb1588ab09fe1
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-27 22:33:34 +0900

    todo

---
+++ todo/main.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector('#login-form');
+const loginInput = loginForm.querySelector('input');
+const loginBtn = loginForm.querySelector('button');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = "hidden";
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    console.log(username);
+    greeting.innerText = "Hello "+username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener('submit',onLoginSubmit);
+
