37e957a7c5125422c3a75bc952f1b4395af2f300
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-29 22:58:32 +0900

    delete todo(id)

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -13,12 +13,15 @@ function saveToDos(){
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos =toDos.filter(toDo=> toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo){
     const li = document.createElement('li');
+    li.id = newTodo.id;
     const span = document.createElement('span');
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement('button');
     button.innerText = '❌';
     button.addEventListener('click',deleteTodo);
@@ -32,8 +35,12 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id : Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -46,3 +53,4 @@ if(savedToDos !== null){
     toDos = parsedToDos;
     parsedToDos.forEach(paintToDo);
 }
+
