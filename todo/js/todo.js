cd36df0b1996cfbaf97c12d79950db0e8e0ede1a
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-29 17:03:20 +0900

    saved todos

--- todo/js/todo.js
+++ todo/js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+const TODOS_KEY = 'todos';
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+}
+
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -24,7 +32,18 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener('submit',handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item)=>{
+        console.log(item)
+    });
+}
