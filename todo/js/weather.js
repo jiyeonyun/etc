60a9e499c4f89f59b52b713ca0559aa49442a237
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-30 16:06:28 +0900

    temp add

--- todo/js/weather.js
+++ todo/js/weather.js
@@ -6,12 +6,15 @@ function onGeoOk(position){
     const lng = position.coords.longitude;
     const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
     fetch(url).then(respose=>respose.json()).then(data=>{
-        const weatherContainer = document.querySelector('#weather span:first-child');
-        const cityContainer = document.querySelector('#weather span:last-child');
+        const weatherContainer = document.querySelector('#weather .weatherContainer');
+        const cityContainer = document.querySelector('#weather .city');
+        const tempContainer = document.querySelector('#weather .temp');
         const name = data.name;
         const weather = data.weather[0].main;
+        const temper = data.main.temp;
         weatherContainer.innerText = name;
         cityContainer.innerText = weather;
+        tempContainer.innerText = `${temper}℃`;
         });
 }
 function onGeoError(){
