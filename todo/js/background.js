b23ba5f16e88a9c64f2786854ce2925ba67ebb4f
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-30 12:57:00 +0900

    update code

--- todo/js/background.js
+++ todo/js/background.js
@@ -1,18 +1,14 @@
 const images = [
-    "01.png",
+    "01.jpg",
     "02.jpg",
     "03.jpg",
     "04.jpg",
     "05.jpg",
     "06.jpg",
-    "07.jpg",
-    "08.jpg"
+    "07.jpg"
 ]
 
 const chosenImage = images[Math.floor(Math.random()*(images.length-1))];
 
-const bgImage= document.createElement('img');
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+const bgImage= document.querySelector('body');
+bgImage.style.backgroundImage = `url('img/${chosenImage}')`;
