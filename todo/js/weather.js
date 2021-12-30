6c7020a85097a742a33a8ad95ad218520dfc5777
jiyeonyun <jiyeonyun43@gmail.com>
2021-12-30 12:07:02 +0900

    weather

--- todo/js/weather.js
+++ todo/js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = 'aaa27313875f00a755edeefc92500459';
+
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("you live in" , lat , lng);
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(respose=>respose.json()).then(data=>{
+        const weatherContainer = document.querySelector('#weather span:first-child');
+        const cityContainer = document.querySelector('#weather span:last-child');
+        const name = data.name;
+        const weather = data.weather[0].main;
+        weatherContainer.innerText = name;
+        cityContainer.innerText = weather;
+        });
 }
 function onGeoError(){
     alert("Can't find you. No weather for you");
