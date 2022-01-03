d432ed9d4c7780b501524c32af734b2e51de73b4
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-03 22:51:46 +0900

    ctx set, painting

--- paintingBorad/app.js
+++ paintingBorad/app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWith = 2.5;
 
 let painting = false;
 
@@ -6,22 +13,28 @@ function stopPainting(){
     painting = false;
 }
 
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-
-function onMouseDouwn(event){
+function startPaining(){
     painting = true;
 }
 
-function onMouseup(event){
-    stopPainting();
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
+    else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
+
+
 
 if(canvas){
     canvas.addEventListener('mousemove',onMouseMove);
-    canvas.addEventListener('mousedown',onMouseDouwn);
-    canvas.addEventListener('mouseup',onMouseup);
+    canvas.addEventListener('mousedown',startPaining);
+    canvas.addEventListener('mouseup',stopPainting);
     canvas.addEventListener('mouseleave',stopPainting);
 }
