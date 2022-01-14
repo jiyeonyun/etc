5e7d1e2dc6e8ee81437172e67222edecf101116f
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-14 13:46:39 +0900

    function

--- 2weeksChallenge/day5/src/index.js
+++ 2weeksChallenge/day5/src/index.js
@@ -1,18 +1,5 @@
 const body = document.querySelector('body');
 
-
-const width = window.outerWidth;
-if(width>=1100){
-    body.style.backgroundColor = '#FFCD00';
-}
-else if(width>=700){
-    body.style.backgroundColor = '#C08EF2';
-}
-else{
-    body.style.backgroundColor = '#70A9F3';
-}
-
-
 function changeBgColors(){
     const width = window.outerWidth;
     if(width>=1100){
@@ -25,5 +12,7 @@ function changeBgColors(){
         body.style.backgroundColor = '#70A9F3';
     }
 }
+
+changeBgColors();
 
 window.addEventListener('resize',changeBgColors);
