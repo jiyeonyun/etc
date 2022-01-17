0b7f27df34eb67f879f2d9c8312947caf3a854d9
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-17 12:11:14 +0900

    set2

---
+++ 2weeksChallenge/day6/src/index.js
@@ -0,0 +1,16 @@
+const Btn = document.querySelector('button');
+const NumberResult = document.querySelector('.resultnum');
+const GameResult = document.querySelector('.gameresult');
+const GenerateNum = document.querySelector('#Gnumber');
+const GuessNum = document.querySelector('#Gusessnum');
+
+
+function RandomNumGame(){
+NumberResult.innerHTML = `You chose : ${} , the machine chose: ${}`
+
+
+
+if(GenerateNum = guse)GameResult.innerHtml = '';
+}
+
+Btn.addEventListener('click',RandomNumGame);
