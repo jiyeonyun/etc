6d037ae89848702521568d0d5bbbe31e1ec8858a
jiyeonyun <jiyeonyun43@gmail.com>
2022-01-21 21:39:28 +0900

    clock

--- 2weeksChallenge/final/clock.js
+++ 2weeksChallenge/final/clock.js
@@ -1,3 +1,23 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
 const loginBtn = loginForm.querySelector('button');
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+const todoform = document.getElementById('todo-form');
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    
+    if(){
+
+    }
+    else{
+
+    }
+}
+
+loginBtn.addEventListener('click',onLoginSubmit);
