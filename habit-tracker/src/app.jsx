18475700a18343eb2ae9bbf29bac9ead590b5f05
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-13 17:28:15 +0900

    form

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -10,14 +10,15 @@ class App extends Component {
         {id: 2, name : 'Running' , count : 0},
         {id: 3, name : 'Coding' , count : 0}
         ],
-};
+      };
       
       handleIncrement = (habit) => {
         const habits = [...this.state.habits];
         const index = habits.indexOf(habit);
         habits[index].count++;
         this.setState({ habits });
-      }
+      };
+
       handleDecrement = (habit) => {
         const habits = [...this.state.habits];
         const index = habits.indexOf(habit);
@@ -31,19 +32,26 @@ class App extends Component {
         this.setState({habits});
         
       };
+
+      handleAdd = name =>{
+        const habits = [...this.state.habits];
+        habits.push({id:Date.now() ,name : name , count : 0})
+        this.setState({habits});
+      }
   render() {
     return (
-      <div>
+      <>
         <Header
-        habits={this.state.habits}
+        totalCount = {this.state.habits.filter(item=>item.count>0).length}
         />
         <Habits 
         habits={this.state.habits}
         onIncrement={this.handleIncrement} 
         onDecrement={this.handleDecrement} 
         onDelete={this.handleDelete}
+        onAdd={this.handleAdd}
         />
-      </div>
+      </>
     );
   }
 }
