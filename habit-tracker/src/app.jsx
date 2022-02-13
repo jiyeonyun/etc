b1f4ca2aaefc9959032e0c68fa935c8e6717454b
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-13 16:39:06 +0900

    props

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -2,6 +2,7 @@ import './app.css';
 import Habits from './components/habits';
 import React, { Component } from 'react';
 import Header from './components/header';
+
 class App extends Component {
     state = {
     habits : [
@@ -10,12 +11,38 @@ class App extends Component {
         {id: 3, name : 'Coding' , count : 0}
         ],
 };
+      
+      handleIncrement = (habit) => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count++;
+        this.setState({ habits });
+      }
+      handleDecrement = (habit) => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+        this.setState({ habits });
+      };
+
+      handleDelete = (habit) => {
+        const habits = this.state.habits.filter(item=>item.id !== habit.id);
+        this.setState({habits});
+        
+      };
   render() {
     return (
       <div>
-        <Header/>
+        <Header
+        habits={this.state.habits}
+        />
         <Habits 
-        habits={this.state.habits} />
+        habits={this.state.habits}
+        onIncrement={this.handleIncrement} 
+        onDecrement={this.handleDecrement} 
+        onDelete={this.handleDelete}
+        />
       </div>
     );
   }
