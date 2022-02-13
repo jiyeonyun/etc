11481be7284469895417bdfe52fcde0ddfc1b9aa
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-13 15:57:28 +0900

    state

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,10 +1,24 @@
 import './app.css';
 import Habits from './components/habits';
-
-function App() {
-  return (
-    <Habits />
-  );
+import React, { Component } from 'react';
+import Header from './components/header';
+class App extends Component {
+    state = {
+    habits : [
+        {id: 1, name : 'Reading' , count : 0},
+        {id: 2, name : 'Running' , count : 0},
+        {id: 3, name : 'Coding' , count : 0}
+        ],
+};
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Habits 
+        habits={this.state.habits} />
+      </div>
+    );
+  }
 }
 
 export default App;
