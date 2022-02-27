c46d26f7eabffcbf5847c3695bd6038e6148722a
jiyeonyun <jiyeonyun43@gmail.com>
2022-02-27 16:26:39 +0900

    keyboard js

--- tutorial/wordle/index.js
+++ tutorial/wordle/index.js
@@ -7,13 +7,28 @@ const resultModal = document.querySelector('.result_modal');
 const resultModalResult = document.querySelector('.result_modal_result');
 const resultmodalTitle = document.querySelector('.resultmodal_title');
 const game = document.querySelector('.game');
+const keyboard = document.querySelector('.keyBorad');
+const color = ['rgb(139 195 74 / 66%)','rgb(255 207 34 / 58%)','rgb(255 59 0 / 54%)','whitesmoke'];
 
-
-
+let colorNum = 0;
 let times = 0;
 let answer = '';
 
-
+function changeColor(e){
+    let target = e.target;
+    console.log(colorNum);
+    
+    if(colorNum <3){
+        target.style.background = `${color[colorNum]}`
+        colorNum=colorNum+1
+    }
+    else if(colorNum == 3){
+        target.style.background = `${color[colorNum]}`
+        colorNum= 0
+        
+        
+    }
+}
 
 function reload(){
     location.reload();
@@ -32,6 +47,7 @@ function play(){
         
     modal.setAttribute('class','modal hide')
     game.setAttribute('class','game')
+    keyboard.setAttribute('class','keyBorad')
 }
 
 
@@ -45,20 +61,21 @@ function answerCheck(){
         }
         else{
             if(realAnswer == answer){
-                game.setAttribute('class','game hide')
-                resultModal.setAttribute('class','result_modal')
+                game.setAttribute('class','game hide');
+                resultModal.setAttribute('class','result_modal');
+                keyboard.setAttribute('class','keyBorad hide');
             }
             else{
                 nullModal.setAttribute('class','.null_modal hide');
             for(let i = 0; i < 5; i++){
                 if(input[i].value == answer[i]){
-                    input[i].style.background = 'rgb(150 227 0)';
+                    input[i].style.background = 'rgb(139 195 74 / 66%)';
                 }
                 else if(answer.includes(input[i].value)){
-                    input[i].style.background = 'rgb(255 207 34)';
+                    input[i].style.background = 'rgb(255 207 34 / 58%)';
                 }
                 else{
-                    input[i].style.background = '#ff3b00';
+                    input[i].style.background = 'rgb(255 59 0 / 54%)';
                 }
                 input[i].classList.remove('answer');
             }
@@ -78,6 +95,7 @@ function answerCheck(){
     }
     else{
         game.setAttribute('class','game hide');
+        keyboard.setAttribute('class','keyBorad hide')
         resultModal.setAttribute('class','result_modal');
         resultModalResult.innerText = `   you lose 👻
         answer is "${answer}"!!`
@@ -88,3 +106,4 @@ function answerCheck(){
 EnterBtn.addEventListener('click',answerCheck);
 modalTitle.addEventListener('click',play);
 resultmodalTitle.addEventListener('click',reload)
+keyboard.addEventListener('click',changeColor);
