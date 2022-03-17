567e831e127d6f9a3e189019fd0443df43c035ac
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-17 15:49:17 +0900

    tracker Add form

---
+++ renewalhabit/src/components/tracker_add_form/tracker_add_form.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './tracker_add_form.module.css';
+
+const TrackerAddForm = (props) => {
+    const inputRef = React.createRef();
+    const onSubmit = (event) =>{
+        event.preventDefault(); 
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
+        inputRef.current.value = '';
+    }
+
+    return (
+        <form className={styles.addForm} onSubmit={onSubmit}>
+            <input 
+                ref={inputRef} 
+                className={styles.addInput}
+                type="text" 
+                placeholder='Write your tracker...' 
+                />
+            <button className={styles.addBtn}>Add</button>
+        </form>
+);
+};
+
+export default TrackerAddForm;
