d63670bf2c70a7a522a99d9993c2bcd79bfcd403
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-17 15:48:56 +0900

    task Tracker

--- renewalhabit/src/components/task_tracker/task_tracker.jsx
+++ renewalhabit/src/components/task_tracker/task_tracker.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import Header from '../header/header';
-import Timer from '../timer/timer';
 import styles from './task_tracker.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from 'react';
+import TrackerAddForm from '../tracker_add_form/tracker_add_form';
+import Trackers from '../trackers/trackers';
+
 const TaskTracker = (props) => {
-    
+    const [full,setFull]  = useState(true);
+    const onClick = ()=>{
+        full ? setFull(false) : setFull(true)
+    }
     return(
-        <div className={styles.task_tracker}>
-        <Header/>
-        <div className={styles.task_trackers}>
-        <Timer/>
+        <div className={styles.taskTracker}>
+            <div className={styles.title}>
+            <p className={styles.titleName}>Task Tracker</p>
+            {
+                full? <FontAwesomeIcon onClick={onClick} className={styles.titleIcon} icon="fa-solid fa-circle-arrow-down" /> 
+                    :<FontAwesomeIcon onClick={onClick} className={styles.titleIcon} icon="fa-solid fa-circle-arrow-up" />
+            }
+            </div>
+            {
+                !full?  <div className={styles.tracker}>
+                            <TrackerAddForm/>
+                            <Trackers/>
+                        </div>  : <></>
+            }
         </div>
-        </div>
-        
     );
 };
 
