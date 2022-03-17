364790c893d5f1e1bbc66a0f56674437cb3799c2
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-17 14:13:05 +0900

    timer

--- renewalhabit/src/components/timer/timer.jsx
+++ renewalhabit/src/components/timer/timer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import styles from './timer.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from 'react';
+
 const Timer = (props) => {
+        const [play,setPlay] = useState(true);
+
     return(
         <div className={styles.timerWrap}>
             <nav className={styles.nav}>
@@ -21,6 +25,21 @@ const Timer = (props) => {
                 <FontAwesomeIcon className={styles.setting} icon="fa-solid fa-gear" />
                 </div>
             </nav>
+            <div className={styles.timer}>
+                <nav className={styles.timerNav}>
+                    <span className={styles.timerBtn}>Focus</span>
+                    <span className={styles.timerTimes}><FontAwesomeIcon className={styles.logo}icon="fa-solid fa-bicycle" />1 / 3</span>
+                    <span className={styles.timerBtn}>Break</span>
+                </nav>
+                <div className={styles.timerClock}>
+                    <h1 className={styles.timerClockNum}>00 : 00 : 00 </h1>
+                    <div className={styles.PlayBtn}>
+                        <div className={styles.forword_back}><FontAwesomeIcon icon="fa-solid fa-backward" /></div>
+                        <div className={styles.Play}>{play ? <FontAwesomeIcon icon="fa-solid fa-play" /> : <FontAwesomeIcon icon="fa-solid fa-pause" />}</div>
+                        <div className={styles.forword_back}><FontAwesomeIcon icon="fa-solid fa-forward" /></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
