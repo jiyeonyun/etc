ff184d4e22dd90c11a6ad5b5258759476234811b
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-17 15:49:32 +0900

    trackers

---
+++ renewalhabit/src/components/trackers/trackers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tracker from '../tracker/tracker';
+import styles from './trackers.module.css';
+const Trackers = (props) => {
+    return(
+        <ul>
+            <Tracker/>
+        </ul>
+    );
+};
+
+export default Trackers;
