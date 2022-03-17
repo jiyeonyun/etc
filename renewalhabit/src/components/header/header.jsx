d163ea2b8e2c7eca2bd47c604cc8ae8a50a59085
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-17 13:43:18 +0900

    header

---
+++ renewalhabit/src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './header.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fabicycle } from "@fortawesome/free-solid-svg-icons";
+const Header = (props) => {
+
+    return(
+        <header className={styles.header}>
+            <div className={styles.headerWrap}>
+            <FontAwesomeIcon className={styles.logo} icon="fa-solid fa-bicycle" />
+            <h1 className={styles.title}>Tracker Cylcle</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
