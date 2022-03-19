9bbcf39d6b1135d657631c5f12c3d05a5285579f
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-20 00:41:19 +0900

    app

--- renewalhabit/src/app.jsx
+++ renewalhabit/src/app.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import styles from './app.module.css';
 import TrackerCycle from './components/tracker_cycle/tracker_cycle';
 
 function App() {
+  const [popup,setPopup] = useState(false);
   return (
-    <div className={styles.wrap}>
-    <TrackerCycle/>
+    <div className={`${popup?styles.wrapPopup:styles.wrap}`}>
+    <TrackerCycle popup={popup} setPopup={setPopup}/>
     </div>
     
   );
