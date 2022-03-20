f1fa188549525fb255f3d7fad30e7a0bf4cffb1f
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-20 20:04:08 +0900

    timer edit

--- renewalhabit/src/app.jsx
+++ renewalhabit/src/app.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import styles from './app.module.css';
+import CycleCopy from './components/tracker_cycle/cycle_copy';
 import TrackerCycle from './components/tracker_cycle/tracker_cycle';
 
 function App() {
   const [popup,setPopup] = useState(false);
   return (
     <div className={`${popup?styles.wrapPopup:styles.wrap}`}>
-    <TrackerCycle popup={popup} setPopup={setPopup}/>
+    <CycleCopy popup={popup} setPopup={setPopup}/>
     </div>
     
   );
--- renewalhabit/src/components/timer/timer_copy.jsx
+++ renewalhabit/src/components/timer/timer_copy.jsx
@@ -3,51 +3,53 @@ import styles from './timer.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState, useRef } from 'react';
 
-const Timer = ({popup,setPopup,cylceTime,BreakMin,BreakHr,focusMin,focusHr}) => {
+const padNumber = (num, length) => {
+    return String(num).padStart(length, '0');
+};
+
+const TimerCopy = (props) => {
         const [play,setPlay] = useState(true);
-        const [start,setStart] = useState(false);
-        const [focus,setFocus] =useState(true);
-        const padNumber = (num, length) => {
-            return String(num).padStart(length, '0');
-        };
-        
-        const tempHour = focus ? focusHr : BreakHr;
-        const tempMin = focus? focusMin : BreakMin;
-        const tempSec = 0;
+        const [cylce,setCycle] = useState(0);
+        const tempHour = props.hour ? parseInt(props.hour) : 0;
+        const tempMin = props.min ? parseInt(props.min) : 0;
+        const tempSec = props.sec ? parseInt(props.sec) : 0;
+        // 타이머를 초단위로 변환한 initialTime과 setInterval을 저장할 interval ref
         const initialTime = useRef(tempHour * 60 * 60 + tempMin * 60 + tempSec);
         const interval = useRef(null);
         const [hour, setHour] = useState(padNumber(tempHour, 2));
         const [min, setMin] = useState(padNumber(tempMin, 2));
         const [sec, setSec] = useState(padNumber(tempSec, 2));
-        console.log(padNumber(tempHour))
-        // useEffect(()=>{
-                
-                
-        //         return () => clearInterval(interval.current);
-        // },[]);
-            const timer = ()=>{
-                setSec(padNumber(initialTime.current % 60, 2));
-                setMin(padNumber(parseInt(initialTime.current / 60), 2));
-                setHour(padNumber(parseInt(initialTime.current / 60 / 60), 2));
+        const timer = () => {
+            interval.current = setInterval(() => {
                 initialTime.current -= 1;
-            }
-        
-            useEffect(() => {
+                setSec(padNumber(initialTime.current % 60, 2));
+                setMin(padNumber(parseInt((initialTime.current % 3600) / 60), 2));
+                setHour(padNumber(parseInt(initialTime.current / 3600), 2));
+            }, 1000);
+                return () => clearInterval(interval.current);
+        }
+
+        // 초가 변할 때만 실행되는 useEffect
+        // initialTime을 검사해서 0이 되면 interval을 멈춘다.
+        useEffect(() => {
             if (initialTime.current <= 0) {
                 clearInterval(interval.current);
+                setCycle(cylce+1);
+                setPlay(!play);
+                props.setFocus(!props.focus);
             }
             }, [sec]);
 
         const onClickPlay = ()=>{
             setPlay(!play);
-            setStart(!start);
-            interval.current = setInterval(() => timer(), 1000);
+            timer();
         }
         const onClickPause = ()=>{
             setPlay(!play);
+            clearInterval(interval.current);
         }
         const onClickPopup = ()=>{
-            setPopup(!popup);
+            props.setPopup(!props.popup);
         }
 
 
@@ -56,15 +58,15 @@ const Timer = ({popup,setPopup,cylceTime,BreakMin,BreakHr,focusMin,focusHr}) => 
             <nav className={styles.nav}>
                 <div className={styles.navdetail}>
                     <span className={styles.navBtn}>Focus</span>
-                    <span className={styles.navDesc}>{`${focusHr?focusHr:0} hr ${focusMin?focusMin:0} min`}</span>
+                    <span className={styles.navDesc}>{`${props.focusHr?props.focusHr:0} hr ${props.focusMin?props.focusMin:0} min`}</span>
                 </div>
                 <div className={styles.navdetail}>
                     <span className={styles.navBtn}>Break</span>
-                    <span className={styles.navDesc}>{`${BreakHr?BreakHr:0} hr ${BreakMin?BreakMin:0} min`}</span>
+                    <span className={styles.navDesc}>{`${props.BreakHr?props.BreakHr:0} hr ${props.BreakMin?props.BreakMin:0} min`}</span>
                 </div>
                 <div className={styles.navdetail}>
                     <span className={styles.navBtn}>Cylces</span>
-                    <span className={styles.navDesc}>{`${cylceTime?cylceTime:0}`}</span>
+                    <span className={styles.navDesc}>{`${props.cylceTime?props.cylceTime:0}`}</span>
                 </div>
                 <div className={styles.navdetail}>
                 <FontAwesomeIcon className={styles.setting} onClick={onClickPopup} icon="fa-solid fa-gear" />
@@ -72,9 +74,9 @@ const Timer = ({popup,setPopup,cylceTime,BreakMin,BreakHr,focusMin,focusHr}) => 
             </nav>
             <div className={styles.timer}>
                 <nav className={styles.timerNav}>
-                    <span className={styles.timerBtn}>Focus</span>
-                    <span className={styles.timerTimes}><FontAwesomeIcon className={styles.logo}icon="fa-solid fa-bicycle" />{`0 / ${cylceTime?cylceTime:0}`}</span>
-                    <span className={styles.timerBtn}>Break</span>
+                    <span className={`${props.focus?styles.timerBtnF:styles.timerBtn}`}>Focus</span>
+                    <span className={styles.timerTimes}><FontAwesomeIcon className={styles.logo}icon="fa-solid fa-bicycle" />{`${cylce} / ${props.cylceTime?props.cylceTime:0}`}</span>
+                    <span className={`${!props.focus?styles.timerBtnF:styles.timerBtn}`}>Break</span>
                     <FontAwesomeIcon className={styles.reset} icon="fa-solid fa-arrow-rotate-right" />
                 </nav>
                 <div className={styles.timerClock}>
@@ -90,4 +92,4 @@ const Timer = ({popup,setPopup,cylceTime,BreakMin,BreakHr,focusMin,focusHr}) => 
     );
 };
 
-export default Timer;
+export default TimerCopy;
