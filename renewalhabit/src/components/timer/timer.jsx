5f8bbc9797af92f62d8cf9d9b5291d2630812d73
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-22 13:52:08 +0900

    timer and cylce

--- renewalhabit/src/components/timer/timer.jsx
+++ renewalhabit/src/components/timer/timer.jsx
@@ -3,90 +3,62 @@ import styles from './timer.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState, useRef } from 'react';
 
-const Timer = ({popup,setPopup,cylceTime,BreakMin,BreakHr,focusMin,focusHr}) => {
-        const [play,setPlay] = useState(true);
-        const [start,setStart] = useState(false);
-        const [focus,setFocus] =useState(true);
-        const padNumber = (num, length) => {
-            return String(num).padStart(length, '0');
-        };
-        
-        const tempHour = focus ? focusHr : BreakHr;
-        const tempMin = focus? focusMin : BreakMin;
-        const tempSec = 0;
-        const initialTime = useRef(tempHour * 60 * 60 + tempMin * 60 + tempSec);
-        const interval = useRef(null);
-        const [hour, setHour] = useState(padNumber(tempHour, 2));
-        const [min, setMin] = useState(padNumber(tempMin, 2));
-        const [sec, setSec] = useState(padNumber(tempSec, 2));
-        console.log(padNumber(tempHour))
-        // useEffect(()=>{
-                
-                
-        //         return () => clearInterval(interval.current);
-        // },[]);
-            const timer = ()=>{
-                setSec(padNumber(initialTime.current % 60, 2));
-                setMin(padNumber(parseInt(initialTime.current / 60), 2));
-                setHour(padNumber(parseInt(initialTime.current / 60 / 60), 2));
-                initialTime.current -= 1;
-            }
-        
-            useEffect(() => {
-            if (initialTime.current <= 0) {
-                clearInterval(interval.current);
-            }
-            }, [sec]);
+const padNumber = (num, length) => {
+    return String(num).padStart(length, '0');
+};
 
-        const onClickPlay = ()=>{
-            setPlay(!play);
-            setStart(!start);
-            interval.current = setInterval(() => timer(), 1000);
-        }
-        const onClickPause = ()=>{
-            setPlay(!play);
-        }
-        const onClickPopup = ()=>{
-            setPopup(!popup);
-        }
+const Timer = (props) => {
+    const onClickPlay = ()=>{
+        props.setPlay(false);
+        props.setPlayBtn(!props.playBtn);
+    }
+    const onClickPause = ()=>{
+        props.setPlay(!props.play);
+        props.setPlayBtn(!props.playBtn);
+    }
+    const onClickPopup = ()=>{
+        props.setPopup(!props.popup);
+    }
+    const resetClick= ()=>{
+        window.location.reload();
+    }
 
-
-        return(
-        <div className={styles.timerWrap}>
-            <nav className={styles.nav}>
-                <div className={styles.navdetail}>
-                    <span className={styles.navBtn}>Focus</span>
-                    <span className={styles.navDesc}>{`${focusHr?focusHr:0} hr ${focusMin?focusMin:0} min`}</span>
-                </div>
-                <div className={styles.navdetail}>
-                    <span className={styles.navBtn}>Break</span>
-                    <span className={styles.navDesc}>{`${BreakHr?BreakHr:0} hr ${BreakMin?BreakMin:0} min`}</span>
-                </div>
-                <div className={styles.navdetail}>
-                    <span className={styles.navBtn}>Cylces</span>
-                    <span className={styles.navDesc}>{`${cylceTime?cylceTime:0}`}</span>
-                </div>
-                <div className={styles.navdetail}>
-                <FontAwesomeIcon className={styles.setting} onClick={onClickPopup} icon="fa-solid fa-gear" />
-                </div>
+    return(
+    <div className={styles.timerWrap}>
+        <nav className={styles.nav}>
+            <div className={styles.navdetail}>
+                <span className={styles.navBtn}>Focus</span>
+                <span className={styles.navDesc}>{`${props.focusHr?props.focusHr:0} hr ${props.focusMin?props.focusMin:0} min`}</span>
+            </div>
+            <div className={styles.navdetail}>
+                <span className={styles.navBtn}>Break</span>
+                <span className={styles.navDesc}>{`${props.BreakHr?props.BreakHr:0} hr ${props.BreakMin?props.BreakMin:0} min`}</span>
+            </div>
+            <div className={styles.navdetail}>
+                <span className={styles.navBtn}>Cylces</span>
+                <span className={styles.navDesc}>{`${props.cylceTime?props.cylceTime:0}`}</span>
+            </div>
+            <div className={styles.navdetail}>
+            <FontAwesomeIcon className={styles.setting} onClick={onClickPopup} icon="fa-solid fa-gear" />
+            </div>
+        </nav>
+        <div className={styles.timer}>
+            <nav className={styles.timerNav}>
+                <span className={`${props.focus?styles.timerBtnF:styles.timerBtn}`}>Focus</span>
+                <span className={styles.timerTimes}><FontAwesomeIcon className={styles.logo}icon="fa-solid fa-bicycle" />{`${props.cylce} / ${props.cylceTime?props.cylceTime:0}`}</span>
+                <span className={`${!props.focus?styles.timerBtnF:styles.timerBtn}`}>Break</span>
+                <FontAwesomeIcon className={styles.reset} onClick={resetClick} icon="fa-solid fa-arrow-rotate-right" />
             </nav>
-            <div className={styles.timer}>
-                <nav className={styles.timerNav}>
-                    <span className={styles.timerBtn}>Focus</span>
-                    <span className={styles.timerTimes}><FontAwesomeIcon className={styles.logo}icon="fa-solid fa-bicycle" />{`0 / ${cylceTime?cylceTime:0}`}</span>
-                    <span className={styles.timerBtn}>Break</span>
-                    <FontAwesomeIcon className={styles.reset} icon="fa-solid fa-arrow-rotate-right" />
-                </nav>
-                <div className={styles.timerClock}>
-                    <h1 className={styles.timerClockNum}>{hour} : {min} : {sec}</h1>
-                    <div className={styles.PlayBtn}>
-                        <div className={styles.forword_back}><FontAwesomeIcon icon="fa-solid fa-backward" /></div>
-                        <div className={styles.Play}>{play ? <FontAwesomeIcon onClick={onClickPlay} icon="fa-solid fa-play" /> : <FontAwesomeIcon onClick={onClickPause} icon="fa-solid fa-pause" />}</div>
-                        <div className={styles.forword_back}><FontAwesomeIcon icon="fa-solid fa-forward" /></div>
-                    </div>
+            <div className={styles.timerClock}>
+                <h1 className={styles.timerClockNum}>{padNumber(props.hour,2)}:{padNumber(props.min,2)}:{padNumber(props.sec,2)}</h1>
+                <div className={styles.PlayBtn}>
+                    <div className={styles.forword_back}><FontAwesomeIcon icon="fa-solid fa-backward" /></div>
+                    <div className={styles.Play}>{props.playBtn ? <FontAwesomeIcon onClick={onClickPlay} icon="fa-solid fa-play" /> : <FontAwesomeIcon onClick={onClickPause} icon="fa-solid fa-pause" />}</div>
+                    <div className={styles.forword_back}><FontAwesomeIcon icon="fa-solid fa-forward" /></div>
                 </div>
             </div>
         </div>
+    </div>
     );
 };
 
