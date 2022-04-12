e888121fa2340959aef71f0807811ed257a19feb
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-12 16:55:23 +0900

    기본타입정리

--- typeScript/index.js
+++ typeScript/index.js
@@ -1,8 +1,16 @@
-let names = 'kim';
-let array = ['kim', 'lee'];
-let obj = { name: 'kim' };
-let numm = '123';
-function 함수(x) {
-    return x * 2;
-}
-let john = [123, false];
+let 이름 = 'kim';
+let 나이 = 50;
+let 결혼했니 = true;
+let 비었니 = null;
+let 회원들 = ['kim', 'park'];
+let 회원들2 = { 사람: '김', 사람2: '박' };
+let 회원1 = 'park';
+let myname = '윤지연';
+let age = 25;
+let where = '대구';
+let favoriteSong = { 가수: '아이브', 곡명: '일레븐' };
+let project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,13 +1,20 @@
-let names :string | number = 'kim';
-let array:string[] = ['kim','lee'];
-let obj:{name:string} = {name :'kim'};
+let 이름:string = 'kim';
+let 나이:number = 50;
+let 결혼했니:boolean = true;
+let 비었니 :null = null;
 
-type ma = string | number;
-let numm: ma = '123';
+let 회원들 :string[] = ['kim','park'];
+let 회원들2:{사람:string,사람2:string } = {사람:'김',사람2:'박'};
+let 회원1 = 'park';
 
-function 함수(x:number) :number{
-    return x *2
+let myname:string = '윤지연';
+let age:number = 25;
+let where:string = '대구';
+
+let favoriteSong :{가수:string,곡명:string} = {가수:'아이브',곡명:'일레븐'};
+
+let project:{member:string[],days:number,started:boolean} = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
 }
-
-type Member = [number,boolean];
-let john:Member = [123,false];
