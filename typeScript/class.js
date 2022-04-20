d7bbbc7a4c3b7bbec5dcbaafa25ec86a14b0f0f5
jiyeonyun <jiyeonyun43@gmail.com>
2022-04-20 14:29:32 +0900

    class

--- typeScript/class.js
+++ typeScript/class.js
@@ -1,7 +1,35 @@
 class Person {
-    constructor() {
-        this.name = 'kim';
+    constructor(a) {
+        this.name = a;
+    }
+    함수(a) {
+        console.log('hi' + a);
     }
 }
-let 사람1 = new Person();
-let 사람2 = new Person();
+let 사람1 = new Person('kim');
+let 사람2 = new Person('park');
+사람1.함수('안녕');
+//숙제
+class Car {
+    constructor(a, b) {
+        this.model = a;
+        this.price = b;
+    }
+    tax() {
+        let result = this.price / 10;
+        return result;
+    }
+}
+//숙제 2
+class Word {
+    constructor(...param) {
+        param.forEach((a) => {
+            if (typeof a == 'string') {
+                this.str.push(a);
+            }
+            else if (typeof a == 'number') {
+                this.num.push(a);
+            }
+        });
+    }
+}
--- typeScript/class.ts
+++ typeScript/class.ts
@@ -1,10 +1,47 @@
 class Person{
     name :string;
-    constructor(){
-        this.name = 'kim';
+    constructor(a:string){
+        this.name = a;
+    }
+    함수(a:string){
+        console.log('hi'+ a );
     }
 }
 
-let 사람1 = new Person();
-let 사람2 = new Person();
+let 사람1 = new Person('kim');
+let 사람2 = new Person('park');
 
+사람1.함수('안녕');
+
+//숙제
+
+class Car{
+    model : string;
+    price : number;
+    constructor(a:string , b:number) {
+        this.model = a;
+        this.price = b;
+    }
+    tax():number{
+        let result = this.price/10;
+        return result;
+    }
+    
+}
+
+//숙제 2
+
+class Word{
+    num: number[];
+    str: string[];
+    constructor(...param){
+        param.forEach((a)=>{
+            if(typeof a == 'string'){
+                this.str.push(a);
+            }
+            else if(typeof a =='number'){
+                this.num.push(a);
+            }
+        });
+    }
+}
